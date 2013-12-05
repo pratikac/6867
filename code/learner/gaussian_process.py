fb634a52c91f17682670ea018ab2f0f9e8c4e747
pratik.ac <pratik.ac@gmail.com>
2013-12-05 11:54:00 -0500

    added tag_cloud stuff

--- code/learner/gaussian_process.py
+++ code/learner/gaussian_process.py
@@ -4,7 +4,7 @@ from get_features import *
 class gaussian_process(): 
     
     def __init__(self, dim, freq, tfidf):
-        pd = process_data(dim, freq, tfidf)
+        self.pd = process_data(dim, freq, tfidf)
         
         self.features = []
         self.labels = []
@@ -13,14 +13,14 @@ class gaussian_process():
         self.pos_features, self.neg_features = [], []
         self.pos_labels, self.neg_labels = [], []
 
-        self.word_freq = pd.word_frequencies
+        self.word_freq = self.pd.word_frequencies
         self.num_words = len(self.word_freq)
         self.word_to_index = dict(zip(self.word_freq.keys(), range(self.num_words)))
         self.index_to_word = dict(zip(range(self.num_words), self.word_freq.keys()))
         for index in range(self.num_words):
-            self.int_to_eng_words[index] = pd.int_to_word[self.index_to_word[index]]
+            self.int_to_eng_words[index] = self.pd.int_to_word[self.index_to_word[index]]
 
-        for dp in pd.data:
+        for dp in self.pd.data:
             z = self.get_bitmap(dp[0])
             self.features.append(z)
             self.labels.append(dp[1])
@@ -53,12 +53,13 @@ class gaussian_process():
         else:
             return -1
 
-gp = gaussian_process(200, -1, 1)
-predictions = []
-for fv in gp.neg_features:
-    t1 = gp.predict(fv)
-    predictions.append(t1)
-    print t1
+def test():
+    gp = gaussian_process(200, -1, 1)
+    predictions = []
+    for fv in gp.neg_features:
+        t1 = gp.predict(fv)
+        predictions.append(t1)
+        print t1
 
-tmp1 = np.array(predictions)
-print np.sum(np.where(tmp1 > 0))/float(len(predictions)) 
+    tmp1 = np.array(predictions)
+    print np.sum(np.where(tmp1 > 0))/float(len(predictions)) 
